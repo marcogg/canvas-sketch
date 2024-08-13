9c095c6cac03269649a32db9af2ce7f252bda73a
Marco Garcia <marcogarcia.gon@gmail.com>
2024-08-13 12:26:50 -0600

    feature: Curves animated and saving to mp4

--- sketch-curves/sketch-curves.js
+++ sketch-curves/sketch-curves.js
@@ -1,14 +1,18 @@
-const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random');
+const canvasSketch = require('canvas-sketch')
+const random = require('canvas-sketch-util/random')
+const math = require('canvas-sketch-util/math')
+const colormap = require('colormap')
+
 
 const settings = {
-  dimensions: [1080, 1080]
+  dimensions: [1080, 1080],
+  animate: true
 };
 
 // There are many arguments comming by default, canvasSketch, height, width, are just a few, we just need to declare them as parameters in the fn
 const sketch = ({ width, height }) => {
-  const cols = 12
-  const rows = 6
+  const cols = 62
+  const rows = 8
   const numCells = cols * rows
 
   // Grid
@@ -22,23 +26,31 @@ const sketch = ({ width, height }) => {
   const my = (height - gh) * 0.5
 
   const points = []
-  let x, y, n
+  let x, y, n, lineWidth, color
   let frequency = 0.002
   let amplitude = 90
+
+  const colors = colormap({
+    colormap: 'magma',
+    nshades: amplitude
+  })
 
   for (let i = 0; i < numCells; i++) {
     x = (i % cols) * cw
     y = Math.floor(i / cols) * ch
 
     n = random.noise2D(x, y, frequency, amplitude)
-    x += n
-    y += n
+    // x += n
+    // y += n
 
-    points.push(new Point({ x, y }))
+    lineWidth = math.mapRange(n, -amplitude, amplitude, 0, 1)
+    color = colors[Math.floor(math.mapRange(n, -amplitude, amplitude, 0, amplitude))]
+
+    points.push(new Point({ x, y, lineWidth, color }))
   }
 
   // This is where we start drawing
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
@@ -48,9 +60,17 @@ const sketch = ({ width, height }) => {
     context.strokeStyle = 'red'
     context.lineWidth = 4
 
+    // update positions
+    points.forEach(point => {
+      n = random.noise2D(point.iX + frame * 3, point.iY, frequency, amplitude)
+      point.x = point.iX + n
+      point.y = point.iY + n
+    })
+
+    let lastX, lastY
+
     // Draw lines
     for (let r = 0; r < rows; r++) {
-      context.beginPath()
 
       for (let c = 0; c < cols - 1; c++) {
         const curr = points[r * cols + c + 0]
@@ -59,14 +79,23 @@ const sketch = ({ width, height }) => {
         const mx = curr.x + (next.x - curr.x) * 0.5;
         const my = curr.y + (next.y - curr.y) * 0.5;
 
-        if (c == 0) context.moveTo(curr.x, curr.y);
-        else if (c == cols - 2) context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
-        else context.quadraticCurveTo(curr.x, curr.y, mx, my);
+        if (!c) {
+          lastX = curr.x
+          lastY = curr.y
+        }
 
-        // if (!c) context.moveTo(point.x, point.y)
-        // else context.lineTo(point.x, point.y)
+        context.beginPath()
+        context.lineWidth = curr.lineWidth
+        context.strokeStyle = curr.color
+
+        context.moveTo(lastX, lastY)
+        context.quadraticCurveTo(curr.x, curr.y, mx, my);
+
+        context.stroke()
+
+        lastX = mx - c / cols * 250
+        lastY = my - r / cols * 250
       }
-      context.stroke()
     }
 
     points.forEach(point => {
@@ -80,9 +109,14 @@ canvasSketch(sketch, settings);
 
 // Class Point
 class Point {
-  constructor({ x, y }) {
+  constructor({ x, y, lineWidth, color }) {
     this.x = x
     this.y = y
+    this.lineWidth = lineWidth
+    this.color = color
+
+    this.iX = x
+    this.iY = y
   }
 
   draw(context) {
